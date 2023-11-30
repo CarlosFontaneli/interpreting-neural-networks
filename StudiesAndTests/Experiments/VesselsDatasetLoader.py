530280007145674be0bca629bc699aa5a8d23a75
fonta42 <fonta42@pop-os.localdomain>
2023-11-30 18:53:02 -0300

    activations experiments

---
+++ StudiesAndTests/Experiments/VesselsDatasetLoader.py
@@ -0,0 +1,66 @@
+import os
+from PIL import Image
+from torch.utils.data import DataLoader, random_split, Dataset
+import torchvision.transforms as transforms
+
+
+class SegmentationDataset(Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.pairs = self._find_pairs(image_dir, mask_dir)
+
+    def _find_pairs(self, image_dir, mask_dir):
+        pairs = []
+        for img_file in os.listdir(image_dir):
+            base_name = os.path.splitext(img_file)[0]
+            mask_file = os.path.join(mask_dir, base_name + '.png')
+            if os.path.exists(mask_file):
+                pairs.append(img_file)
+        return pairs
+
+    def __len__(self):
+        return len(self.pairs)
+
+    def __getitem__(self, idx):
+        img_file = self.pairs[idx]
+        image_path = os.path.join(self.image_dir, img_file)
+        base_name = os.path.splitext(img_file)[0]
+        mask_path = os.path.join(self.mask_dir, base_name + '.png')
+
+        image = Image.open(image_path)
+        mask = Image.open(mask_path)
+
+        if self.transform:
+            image = self.transform(image)
+            mask = self.transform(mask)
+
+        return image, mask
+
+
+def get_dataset(batch_size = 8):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.CenterCrop((448, 448)),
+    ]) 
+
+    # Paths to your image and mask directories
+    image_dir = "../data/images/"
+    mask_dir = '../data/labels/'
+
+    # Create the dataset
+    dataset = SegmentationDataset(image_dir, mask_dir, transform)
+
+
+    dataset_size = len(dataset)
+    train_size = int(dataset_size * 0.8)  # e.g., 80% for training
+    val_size = dataset_size - train_size
+
+    # Split the dataset
+    train_dataset, train_dataset = random_split(dataset, [train_size, val_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+    
+    return train_loader, test_loader
--- StudiesAndTests/Models/CustomCNNVessel.py
+++ StudiesAndTests/Models/CustomCNNVessel.py
@@ -48,30 +48,82 @@ class CustomResNet(nn.Module):
         self.final = nn.Conv2d(64, num_classes, kernel_size=1)
         
     def forward(self, x):
-      # Encoder
-      x1 = self.conv1(x)
-      x1 = self.bn1(x1)
-      x1 = self.relu(x1)
-      x2 = self.resblock1(x1)
-      x3 = self.resblock2(x2)
-      x4 = self.resblock3(x3)
-      x5 = self.resblock4(x4)
-      
-      x5 = self.dropout(x5)
-      
-      # Decoder
-      x = self.upconv1(x5)  # Upsampling
-      
-      x = x + x4         
-      x = self.upconv2(x)  
-      x = x + x3         
-      x = self.upconv3(x)  
-      x = x + x2         
-      x = self.upconv4(x)  
-      x = x + x1         
-      
-      
-      # Final Layer
-      x = self.final(x)
+        # Encoder
+        x1 = self.conv1(x)
+        x1 = self.bn1(x1)
+        x1 = self.relu(x1)
+        x2 = self.resblock1(x1)
+        x3 = self.resblock2(x2)
+        x4 = self.resblock3(x3)
+        x5 = self.resblock4(x4)
+        
+        x5 = self.dropout(x5)
+        
+        # Decoder
+        x = self.upconv1(x5)  # Upsampling
+        
+        x = x + x4         
+        x = self.upconv2(x)  
+        x = x + x3         
+        x = self.upconv3(x)  
+        x = x + x2         
+        x = self.upconv4(x)  
+        x = x + x1         
+        
+        
+        # Final Layer
+        x = self.final(x)
+        
+        return x
     
-      return x
+    def forward_activation(self, x):
+        self.activations = {}  
+
+        # Encoder
+        x1 = self.conv1(x)
+        self.activations['x1'] = x1
+        x1_bn = self.bn1(x1)
+        self.activations['x1_bn'] = x1_bn
+        x1_relu = self.relu(x1_bn)
+        self.activations['x1_relu'] = x1_relu
+
+        x2 = self.resblock1(x1_relu)
+        self.activations['x2'] = x2
+        x3 = self.resblock2(x2)
+        self.activations['x3'] = x3
+        x4 = self.resblock3(x3)
+        self.activations['x4'] = x4
+        x5 = self.resblock4(x4)
+        self.activations['x5'] = x5
+
+        x5_drop = self.dropout(x5)
+        self.activations['x5_drop'] = x5_drop
+
+        # Decoder
+        x = self.upconv1(x5_drop)  # Upsampling
+        self.activations['upconv1'] = x
+
+        x_1 = x + x4
+        self.activations['skip1'] = x_1
+        x_2 = self.upconv2(x_1)
+        self.activations['upconv2'] = x_2
+
+        x_3 = x_2 + x3
+        self.activations['skip2'] = x_3
+        x_4 = self.upconv3(x_3)
+        self.activations['upconv3'] = x_4
+
+        x_5 = x_4 + x2
+        self.activations['skip3'] = x_5
+        x_6 = self.upconv4(x_5)
+        self.activations['upconv4'] = x_6
+
+        x_7 = x_6 + x1
+        self.activations['skip4'] = x_7
+
+        # Final Layer
+        x_final = self.final(x_7)
+        self.activations['x_final'] = x_final
+
+        return self.activations
+
