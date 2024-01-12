7662c351129c72df721b23c3b38a8d0e95a36a42
fonta42 <fonta42@pop-os.localdomain>
2024-01-12 12:12:15 -0300

    updating kernel=3 padding=1 output_padding=1

--- models/CustomCNNVessel.py
+++ models/CustomCNNVessel.py
@@ -10,7 +10,7 @@ class ResidualBlock(nn.Module):
         self.skip = nn.Sequential()
         if in_channels != out_channels or stride != 1:
             self.skip = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=stride, bias=False),
                 nn.BatchNorm2d(out_channels)
             )
             
@@ -38,14 +38,15 @@ class CustomResNet(nn.Module):
         self.dropout = nn.Dropout(0.5)
         
         # Decoder
-        self.upconv1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-        self.upconv2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.upconv3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.upconv4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.upconv1 = nn.ConvTranspose2d(1024, 512, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.upconv2 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.upconv3 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.upconv4 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1)
+
         
         
         # Final Layer
-        self.final = nn.Conv2d(64, num_classes, kernel_size=1)
+        self.final = nn.Conv2d(64, num_classes, kernel_size=3, stride=1, padding=1)
         
     def forward(self, x):
         # Encoder
